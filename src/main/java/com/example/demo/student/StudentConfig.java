77b26bb19eb56f585529fbbdbda91986477590d7
fjjt3 <fjjt3@yahoo.com>
2023-04-11 12:25:50 +0200

    added feature save

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class StudentConfig {
+  @Bean
+  CommandLineRunner commandLineRunner(StudentRepository repository){
+    return args -> {
+      new Student marian = new Student(
+        name = "Marian",
+        email = "mariam.jamal@gmail.com",
+        LocalDate.of(year = 2000, JANUARY, dayOfMonth = 5),
+        age = 23
+      );
+      new Student alex = new Student(
+        name = "Alex",
+        email = "alex@gmail.com",
+        LocalDate.of(year = 2005, JANUARY, dayOfMonth = 5),
+        age = 21
+      );
+
+      repository.saveAll(
+        List.of(marian, alex)
+      );
+    };
+
+
+
+    };
+  }
+
