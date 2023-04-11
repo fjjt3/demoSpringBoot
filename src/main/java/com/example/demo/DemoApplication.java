6b54a525cb257d0b3829e19f1d00b0d20bc124a9
fjjt3 <fjjt3@yahoo.com>
2023-04-11 09:22:11 +0200

    hello student

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import com.example.demo.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+
+    @GetMapping
+    public List<Student> hello() {
+      return List.of(
+        new Student(
+          id:1L,
+          name:"Marian",
+          email:"mariam.jamal@gmail.com",
+          LocalDate.of(year:2000, Month.JANUARY, dayOfMonth:5),
+          age:23
+
+        )
+    );
+    }
+  }
+}
